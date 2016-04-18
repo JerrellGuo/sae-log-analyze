37fb980b4b1dee4084d807ee579cf3c7f46e5919
JerrelGuo <jiaxiang.guo@qq.com>
2016-04-18 21:11:20 +0800

    first comit

---
+++ sae-log-analyze.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+import urllib2
+from apibus_handler import *
+ACCESSKEY = 'your accesskey'
+SECRETKEY = 'your secretkey'
+apibus_handler = SaeApibusAuthHandler(ACCESSKEY, SECRETKEY)
+opener = urllib2.build_opener(apibus_handler)
+log_file = opener.open('http://g.sae.sina.com.cn/log/http/2016-04-18/1-access.log') #这里参考api文档说明填写
+
+ips = []
+for line in log_file:
+    array = line.split(' ')
+    ips.append(array[1])
+
+ips.sort()
+for i in range(len(ips)):
+    print ips[i]
