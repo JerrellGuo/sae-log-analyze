de31d32c35e729bc5518dd5ad0696ce720076efb
JerrellGuo <jiaxiang.guo@qq.com>
2016-04-18 21:16:03 +0800

    Update sae-log-analyze.py

--- sae-log-analyze.py
+++ sae-log-analyze.py
@@ -5,7 +5,7 @@ ACCESSKEY = 'your accesskey'
 SECRETKEY = 'your secretkey'
 apibus_handler = SaeApibusAuthHandler(ACCESSKEY, SECRETKEY)
 opener = urllib2.build_opener(apibus_handler)
-log_file = opener.open('http://g.sae.sina.com.cn/log/http/2016-04-18/1-access.log') #这里参考api文档说明填写
+log_file = opener.open('http://g.sae.sina.com.cn/log/http/2016-04-18/1-access.log') #这里参考api文档说明填写，,我这里获取的是4月18号的所有http访问日志
 
 ips = []
 for line in log_file:
